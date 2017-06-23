30a3898e303a825f44e015f092e038219b673816
John Scott <johnlscott@openmailbox.org>
2017-06-23 19:21:27 -0400

    Include <stdlib.h>

--- bin/turf.c
+++ bin/turf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-int main( int argc, char *argv[] )
+#include <stdlib.h>
+int main(int argc, char *argv[])
 {
 	char out[] = "torsocks 'surf ";
 	char argz[31];
